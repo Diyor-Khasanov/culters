87b4cbd10fe012f7edf839de424279168213c488
Diyor <baxacap@gmail.com>
2025-05-27 13:21:23 +0500

    dahsboard html

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import PanelClose from "./img/Panel-close.png";
 import mail from "./img/mail.png";
 import notifications from "./img/notifications.png";
 import admin from "./img/admin.png";
+import company from "./img/company.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -36,7 +38,48 @@ function Home() {
         </div>
       </div>
       <div className="hero">
-        <div className="dashboard"></div>
+        <div className="dashboard">
+          <div className="company">
+            <img src={company} alt="company" />
+            <div className="text">
+              <p className="subtitle">Company</p>
+              <h3 className="title">Kanky Store</h3>
+            </div>
+          </div>
+
+          <h2 className="title">General</h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/">Transaction</Link>
+            </li>
+            <li>
+              <Link to="/customer">Customers</Link>
+            </li>
+            <li>
+              <Link to="/"> Sales report</Link>
+            </li>
+          </ul>
+          <h2 className="title">TOOLS</h2>
+          <ul>
+            <li>
+              <Link to="/"> Account & Settings</Link>
+            </li>
+            <li>
+              <Link to="/"> Help</Link>
+            </li>
+            <li>
+              Dark Mode 
+              <input type="checkbox" />
+            </li>
+            <li>Guy Hawkins</li>
+          </ul>
+        </div>
         <div className="main"></div>
       </div>
     </div>
